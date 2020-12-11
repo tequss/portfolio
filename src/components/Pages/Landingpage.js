8b112c79e9823bfb971a1140c4fe8d0b108e11e4
tequs <witek10012@wp.pl>
2020-12-11 19:04:52 +0100

    fixing

--- src/components/Pages/Landingpage.js
+++ src/components/Pages/Landingpage.js
@@ -17,9 +17,11 @@ class Landing extends Component{
             <Router>
                 {/* <div id="landing"> */}
                 
-                <h1>Welcome on WK-Dev Page</h1>
-                <img src="svg-2.svg" alt="image1" width="100px"/>
-                <h2>We are what you are looking for</h2>
+                <h1>Welcome </h1>
+                <h1> on </h1>
+                <h1>WK-Developer</h1>
+                {/* <img src="svg-2.svg" alt="image1" width="100px"/> */}
+                {/* <h2>We are what you are looking for</h2> */}
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 {/* </div> */}
                 <Footer />
