cd59b325fd44356e940339060c15caa510507984
tequs <witek10012@wp.pl>
2020-12-23 17:11:22 +0100

    Adding new contact form style

--- src/components/Pages/ContactMe.js
+++ src/components/Pages/ContactMe.js
@@ -22,19 +22,10 @@ class ContactMe extends Component {
                                     .start();
                             }}
                         />
-                        {/* <div className="contact">
-                            <form className="contact-form">
-                                <label>Name</label>
-                                <input type="text" name="Twoje Imię i Nazwisko" value="" />
-                                <label>Email</label>
-                                <input type="text" name="Twoje Imię i Nazwisko" value="" />
-                                <label>Wiadomość</label>
-                                <input type="text" name="Twoje Imię i Nazwisko" value="" />
-                            </form>
-                        </div> */}
-                        <ContactForm/>
 
-                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+                        <ContactForm />
+
+
                     </section>
                 </div>
 
